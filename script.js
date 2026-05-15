f06ae72c13d069f2366e9c4631d21751053eb3b2
nolenphya <64279075+nolenphya@users.noreply.github.com>
2026-05-15 01:00:01 -0400

    2026.05.15_12:59AM

--- script.js
+++ script.js
@@ -108,7 +108,7 @@ function createZipBasedChoropleth(data, neighborhoods, artistGroups) {
   });
 
   neighborhoods.features.forEach(f => { 
-    f.properties.artistCount = Number(countsMap[ntaname]) || 0;
+    f.properties.artistCount = Number(countsMap[f.properties.ntaname]) || 0; 
   });
   
 const maxArtists = Math.max(...Object.values(countsMap), 1);
